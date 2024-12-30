3bc6a3864836bdd5cd6fdfe627b6bcf7bc6960e4
Calvin Crane <1049799+landed1@users.noreply.github.com>
2024-12-30 10:25:55 +0000

    adding export to config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {
   // Allow .mdx extensions for files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  output: "export",
 };
 
 const withMDX = createMDX({
